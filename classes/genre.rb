392422ae54d4be87485084b120c6a18980d9215a
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-05 09:57:51 +0100

    Fix all linters errors

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,13 +1,14 @@
 class Genre
-    attr_accessor :items
-    def initialize(name)
-        @name = name
-        @id = rand 1..1000
-        @items = []
-    end
+  attr_accessor :items
 
-    def add_item(item)
-        @items.push(item) unless @items.include?(item)
-        item.add_genre(self)
-    end
+  def initialize(name)
+    @name = name
+    @id = rand 1..1000
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item) unless @items.include?(item)
+    item.add_genre(self)
+  end
 end
--- classes/musicalbum.rb
+++ classes/musicalbum.rb
@@ -1,14 +1,14 @@
 require_relative 'item'
 
 class MusicAlbum < Item
-    attr_accessor :on_spotify
+  attr_accessor :on_spotify
 
-    def initialize(published_date, on_spotify: true, archived: false)
-        super(published_date, archived: archived)
-        @on_spotify = on_spotify
-    end
+  def initialize(published_date, on_spotify: true, archived: false)
+    super(published_date, archived: archived)
+    @on_spotify = on_spotify
+  end
 
-    def can_be_archived?
-        super && @on_spotify
-    end
+  def can_be_archived?
+    super && @on_spotify
+  end
 end
