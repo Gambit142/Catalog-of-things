379c3343dfbfcb0e279bc6876a3a07009b5a6764
wuyepabdul <wuyepabdul@gmail.com>
2022-01-05 11:30:55 +0100

    Add author class and its methods

--- classes/game.rb
+++ classes/game.rb
@@ -1,19 +1,19 @@
 require_relative 'item'
+require 'date'
 
 class Game < Item
     attr_accessor :multiplayer, :last_played_at
 
-    def initialize(multiplayer,last_played_at,published_date)
+    def initialize(multiplayer, last_played_at, published_date)
         super(published_date)
         @multiplayer = multiplayer
-        @last_played_at = last_played_at
+        @last_played_at = Date.parse(last_played_at)
     end
 
     def can_be_archived?
-        super || @last_played_at == 'bad'
+        super && DateTime.now.year - @last_played_at.year > 2
     end
 end
 
-# game = Game.new('game_of_thrones','2021/12/12','2020/12/12')
-# puts game.cover_state
-# puts game.can_be_archived?
+game = Game.new('game_of_thrones','2021/12/12','2020/12/12')
+puts game.can_be_archived?
