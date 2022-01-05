6ed95cc6dac43f3bba9be226a56f3a00caed55d6
wuyepabdul <wuyepabdul@gmail.com>
2022-01-05 14:48:45 +0100

    Fix linters

--- classes/author.rb
+++ classes/author.rb
@@ -1,21 +1,21 @@
 require_relative 'item'
 
 class Author
-    attr_accessor :first_name, :last_name, :items
+  attr_accessor :first_name, :last_name, :items
 
-    def initialize(first_name, last_name)
-        @id = Random.rand(1..1000)
-        @first_name = first_name
-        @last_name = last_name
-        @items = []
-    end
+  def initialize(first_name, last_name)
+    @id = Random.rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
 
-    def add_item(item)
-        if @items.include?(item)
-            puts 'item already exists'
-        else
-            @items.push(item)
-            item.author = self
-        end
+  def add_item(item)
+    if @items.include?(item)
+      puts 'item already exists'
+    else
+      @items.push(item)
+      item.author = self
     end
+  end
 end
--- classes/game.rb
+++ classes/game.rb
@@ -2,15 +2,15 @@ require_relative 'item'
 require 'date'
 
 class Game < Item
-    attr_accessor :multiplayer, :last_played_at
+  attr_accessor :multiplayer, :last_played_at
 
-    def initialize(multiplayer, last_played_at, published_date)
-        super(published_date)
-        @multiplayer = multiplayer
-        @last_played_at = Date.parse(last_played_at)
-    end
+  def initialize(multiplayer, last_played_at, published_date)
+    super(published_date)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+  end
 
-    def can_be_archived?
-        super && DateTime.now.year - @last_played_at.year > 2
-    end
+  def can_be_archived?
+    super && DateTime.now.year - @last_played_at.year > 2
+  end
 end
