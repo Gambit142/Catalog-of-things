cac9b60476e30da95f88cb2d07ffd51a27504ce8
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-06 18:31:40 +0100

    Create methods that will save data to and read data from the json files for musicalbums

--- modules/musicalbum_storage.rb
+++ modules/musicalbum_storage.rb
@@ -1,2 +1,21 @@
+require 'json'
+require_relative '../classes/musicalbum'
+require_relative '../classes/label'
+
 module MusicAlbumStorage
+    def read_musicalbums
+        return unless File.exist?('./json/musicalbum.json')
+        file = File.read('./json/musicalbum.json')
+        file_data = JSON.parse(file)
+        file_data.each do |album|
+            album_instance = MusicAlbum.new(album['date'], on_spotify: album['on_spotify'])
+            label_instance = Label.new(album['title'], album['color'])
+            album_instance.label = label_instance
+            @musicalbums.push(album_instance)
+        end
+    end
+
+    def save_album
+        File.write('./json/musicalbum.json', JSON.dump(@musicalbums))
+    end
 end
