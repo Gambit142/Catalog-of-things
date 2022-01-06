7e919578d3f07ca370c665732b883dafe0e69924
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-06 18:30:41 +0100

    Create methods that will save data to and read data from the json files for genres

--- modules/genre_storage.rb
+++ modules/genre_storage.rb
@@ -1,2 +1,18 @@
+require 'json'
+require_relative '../classes/genre'
+
 module GenreStorage
+    def save_genre
+        File.write('./json/genre.json', JSON.dump(@genres))
+    end
+
+    def read_genres
+        return unless File.exist?('./json/genre.json')
+        file = File.read('./json/genre.json')
+        file_data = JSON.parse(file)
+        file_data.each do |genre|
+          genre_instance = Genre.new(genre['name'])
+          @genres.push(genre_instance)
+      end
+    end
 end
