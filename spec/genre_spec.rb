0708bf422d2c573bd0191642f9582fce0eb703b5
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-06 21:31:09 +0100

    Test the behaviour of the Genre class

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,27 +1,23 @@
 require './classes/genre'
+require './classes/book'
 
 describe Genre do
     before(:each) do
-      @genre1 = Genre.new('2000/10/03', on_spotify: true)
-
+      @genre = Genre.new('Hip-Hop')
+      book = Book.new('Macmillian', 'Good', '2020-10-02')
+      @genre.add_item(book)
     end
   
     it 'should have its own attributes' do
-      expect(@album1).to have_attributes(published_date: Date.parse('2000/10/03'))
-      expect(@album1).to have_attributes(on_spotify: true)
-      expect(@album2).to have_attributes(published_date: Date.parse('2019/10/03'))
-      expect(@album2).to have_attributes(on_spotify: true)
+      expect(@genre).to have_attributes(name: "Hip-Hop")
     end
   
     it 'should print a string when the to_s function is called' do
-      string1 = @album1.to_s
-      string2 = @album2.to_s
-      expect(string1).to eql "Music Album name: Roc Nation\npublished date: 2000-10-03\nHosted on Spotify: true"
-      expect(string2).to eql "Music Album name: AfterMath\npublished date: 2019-10-03\nHosted on Spotify: true"
+      string = @genre.to_s
+      expect(string).to eql "Name of Genre: Hip-Hop\nBook"
     end
   
-    it 'should be archived if certain conditions are met' do
-      expect(@album1.can_be_archived?).to eql true
-      expect(@album2.can_be_archived?).to eql false
-    end
+    it 'should be an instance of Genre' do
+        expect(@genre).to be_instance_of Genre
+      end
   end
