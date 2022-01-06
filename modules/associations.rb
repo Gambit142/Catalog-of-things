0d5320f25270fca216f1bf94a71a688cebfc6b89
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-01-06 14:45:33 +0100

    Fix all linters error for this branch

--- modules/associations.rb
+++ modules/associations.rb
@@ -4,47 +4,46 @@ require_relative '../classes/genre'
 require_relative '../classes/author'
 
 module Associations
-    def add_label_info(item)
-        print 'What is the label of the item?'
-        print 'title:'
-        title = gets.chomp
-        item.label = title
-        
-        print 'color:'
-        color= gets.chomp
-        
-        label = Label.new(title,color)
-    
-        label.add_item(item)
-        
-        @labels << label
-    end
+  def add_label_info(item)
+    print 'What is the label of the item?'
+    print 'title:'
+    title = gets.chomp
+    item.label = title
 
-    def add_genre_info(item)
-        print 'What is the genre of the item?'
-        print 'name:'
-        genre_name = gets.chomp
-        
-        genre = Genre.new(genre_name)
-    
-        genre.add_item(item)
-        
-        @genres << genre
-    end
+    print 'color:'
+    color = gets.chomp
 
-    def add_author_info(item)
-        print 'Who is the author of the item?'
-        print 'first name :'
-        first_name = gets.chomp
-        
-        print 'last name:'
-        last_name= gets.chomp
-        
-        author = Author.new(first_name,last_name)
-    
-        author.add_item(item)
-        
-        @authors << author
-    end
+    label = Label.new(title, color)
 
+    label.add_item(item)
+
+    @labels << label
+  end
+
+  def add_genre_info(item)
+    print 'What is the genre of the item?'
+    print 'name:'
+    genre_name = gets.chomp
+
+    genre = Genre.new(genre_name)
+
+    genre.add_item(item)
+
+    @genres << genre
+  end
+
+  def add_author_info(item)
+    print 'Who is the author of the item?'
+    print 'first name :'
+    first_name = gets.chomp
+
+    print 'last name:'
+    last_name = gets.chomp
+
+    author = Author.new(first_name, last_name)
+
+    author.add_item(item)
+
+    @authors << author
+  end
 end
