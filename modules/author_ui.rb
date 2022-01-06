04ef8c35de368326a16fa116e1a1362cb426841b
wuyepabdul <wuyepabdul@gmail.com>
2022-01-06 14:08:19 +0100

    Create author_ui module and add display author method

---
+++ modules/author_ui.rb
@@ -0,0 +1,16 @@
+module AuthorUI
+    def display_author
+      if @authors.length.zero?
+        puts 'No authors registered yet!'
+      else
+  
+        puts "List of all Authors: \n"
+        @authors.each_with_index do |author, index|
+          print "#{index + 1} game :"
+          print "title: #{author.title}"
+          print "color: #{author.color}"
+        end
+      end
+    end
+end
+  
