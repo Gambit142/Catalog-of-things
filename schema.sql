6c9a3c70effc48f9ffe2636c9bd037273cfb21c9
wuyepabdul <wuyepabdul@gmail.com>
2022-01-07 15:09:29 +0100

    Delete games_and_author sql file and add game and author table in schema.sql file

--- schema.sql
+++ schema.sql
@@ -7,6 +7,21 @@ CREATE TABLE IF NOT EXISTS genre (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE IF NOT EXISTS label(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50),
+    color VARCHAR(50),
+    items INT []
+);
+
+CREATE TABLE IF NOT EXISTS author(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  items INT [],
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE IF NOT EXISTS musicalbum (
     id SERIAL PRIMARY KEY,
     on_spotify BOOLEAN,
@@ -26,7 +41,21 @@ CREATE TABLE IF NOT EXISTS musicalbum (
 	    REFERENCES author(id)
 );
 
-CREATE TABLE IF NOT EXISTS Game(
+CREATE TABLE IF NOT EXISTS book(
+    id SERIAL PRIMARY KEY,
+    publisher VARCHAR(100),
+    cover_state VARCHAR(100),
+    published_date date,
+    archived boolean,
+    label_id INT,
+    author_id INT,
+    genre_id INT,
+    CONSTRAINT label_id FOREIGN KEY(label_id) REFERENCES label(id),
+    CONSTRAINT author_id FOREIGN KEY(author_id) REFERENCES author(id),
+    CONSTRAINT genre_id FOREIGN KEY(genre_id) REFERENCES genre(id)
+);
+
+CREATE TABLE game(
   id INT GENERATED ALWAYS AS IDENTITY,
   publish_date DATE,
   author_id INT,
@@ -34,15 +63,16 @@ CREATE TABLE IF NOT EXISTS Game(
   last_played_at DATE,
   multiplayer BOOLEAN,
   PRIMARY KEY (id),
-  CONSTRAINT fk_game
-    FOREIGN KEY(author_id) 
-	    REFERENCES Author(id)
-	      ON DELETE SET NULL
-);
-
-CREATE TABLE Author(
-  id INT GENERATED ALWAYS AS IDENTITY,
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
-  PRIMARY KEY (id)
+  genre_id INT,
+  label_id INT,
+  author_id INT,
+  CONSTRAINT fk_genre
+  FOREIGN KEY(genre_id)
+    REFERENCES genre(id),
+  CONSTRAINT fk_label
+  FOREIGN KEY(label_id)
+    REFERENCES label(id),
+  CONSTRAINT fk_author
+  FOREIGN KEY(author_id)
+    REFERENCES author(id)
 );
