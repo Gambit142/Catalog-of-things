943a5cf1a0fe3bc41c67dab10663fa03ca2389bc
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-07 13:19:17 +0100

    creating book table in schema.sql

--- schema.sql
+++ schema.sql
@@ -32,3 +32,19 @@ CREATE TABLE IF NOT EXISTS musicalbum (
     -- FOREIGN KEY(author_id)
 	--     REFERENCES author(id)
 );
+
+
+CREATE TABLE book(
+    id SERIAL PRIMARY KEY,
+    publisher VARCHAR(100),
+    cover_state VARCHAR(100),
+    published_date date,
+    archived boolean,
+    label_id INT,
+    -- author_id INT,
+    genre_id INT,
+    CONSTRAINT label_id FOREIGN KEY(label_id) REFERENCES label(id),
+    -- CONSTRAINT author_id FOREIGN KEY(author_id) REFERENCES author(id),
+    CONSTRAINT genre_id FOREIGN KEY(genre_id) REFERENCES genre(id)
+);
+
