bb8262f96a76a5952ce18fe0b10b37abfe051301
Hichami Houssam <hichami.houssam@gmail.com>
2022-01-07 13:18:09 +0100

    creating label table in schema.sql

--- schema.sql
+++ schema.sql
@@ -7,20 +7,27 @@ CREATE TABLE IF NOT EXISTS genre (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE IF NOT EXISTS label(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50),
+    color VARCHAR(50),
+    items INT [],
+);
+
 CREATE TABLE IF NOT EXISTS musicalbum (
     id SERIAL PRIMARY KEY,
     on_spotify BOOLEAN,
     published_date DATE,
     archived BOOLEAN,
     genre_id INT,
-    -- label_id INT,
+    label_id INT,
     -- author_id INT,
     CONSTRAINT fk_genre
     FOREIGN KEY(genre_id)
 	    REFERENCES genre(id)
-    -- CONSTRAINT fk_label
-    -- FOREIGN KEY(label_id)
-	--     REFERENCES label(id),
+    CONSTRAINT fk_label
+    FOREIGN KEY(label_id)
+	    REFERENCES label(id),
     -- CONSTRAINT fk_author
     -- FOREIGN KEY(author_id)
 	--     REFERENCES author(id)
