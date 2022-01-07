8600ff32d77cd4c5f3d8a8d204a0ba8131de547e
wuyepabdul <wuyepabdul@gmail.com>
2022-01-07 14:55:50 +0100

    Add game and author tables

--- schema.sql
+++ schema.sql
@@ -13,15 +13,36 @@ CREATE TABLE IF NOT EXISTS musicalbum (
     published_date DATE,
     archived BOOLEAN,
     genre_id INT,
-    -- label_id INT,
-    -- author_id INT,
+    label_id INT,
+    author_id INT,
     CONSTRAINT fk_genre
     FOREIGN KEY(genre_id)
 	    REFERENCES genre(id)
-    -- CONSTRAINT fk_label
-    -- FOREIGN KEY(label_id)
-	--     REFERENCES label(id),
-    -- CONSTRAINT fk_author
-    -- FOREIGN KEY(author_id)
-	--     REFERENCES author(id)
+    CONSTRAINT fk_label
+    FOREIGN KEY(label_id)
+	    REFERENCES label(id),
+    CONSTRAINT fk_author
+    FOREIGN KEY(author_id)
+	    REFERENCES author(id)
+);
+
+CREATE TABLE IF NOT EXISTS Game(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  publish_date DATE,
+  author_id INT,
+  archived BOOLEAN,
+  last_played_at DATE,
+  multiplayer BOOLEAN,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_game
+    FOREIGN KEY(author_id) 
+	    REFERENCES Author(id)
+	      ON DELETE SET NULL
+);
+
+CREATE TABLE Author(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  PRIMARY KEY (id)
 );
